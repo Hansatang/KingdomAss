d6075cedd427f7556dbd5d12f0914c24ae88b901
dorinpascal <74955991+dorinpascal@users.noreply.github.com>
2021-05-03 16:05:24 +0200

    Dorin

--- src/Accountant.java
+++ src/Accountant.java
@@ -1,9 +1,51 @@
-public class Accountant implements Runnable
-{
+import Valuables.Valuable;
+
+import java.util.ArrayList;
+
+public class Accountant implements Runnable {
+    private TreasureRoomDoor treasureRoomDoor;
+    private int sum;
+    private ArrayList<Valuable> list;
+    private Logger logger;
+
+    public Accountant(TreasureRoomDoor treasureRoomDoor) {
+        this.treasureRoomDoor=treasureRoomDoor;
+        sum=0;
+        
+
+    }
+
     @Override
     public void run() {
 
+        while (true) {
+            
+            
+            treasureRoomDoor.aqquireRead();
+            spendTime(1000);
+            for (int i = 0; i < treasureRoomDoor.getSize; i++) {
 
+                sum+=list.get(i).getWorth();
+
+            }
+
+            logger.log(String.valueOf(sum));
+            spendTime(1000);
+
+        }
 
     }
+
+    public void spendTime(int sleepTime)
+    {
+        try
+        {
+            Thread.sleep(sleepTime);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 }
