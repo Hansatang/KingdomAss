0da6a391107116cb381c1ec5d8c90bbe1fbd422f
dorinpascal <74955991+dorinpascal@users.noreply.github.com>
2021-05-03 15:32:09 +0200

    Dorin

--- src/Guard.java
+++ src/Guard.java
@@ -1,7 +1,34 @@
-public class Guard implements Runnable
-{
-  @Override public void run()
-  {
+import Valuables.Valuable;
 
+import java.util.ArrayList;
+
+public class Guard implements TreasureRoomDoor
+{
+  private ArrayList<Valuable> valuables;
+
+  public Guard() {
+    valuables = new ArrayList<>();
+  }
+  @Override
+  public void addValuables(ArrayList<Valuable> listOfValuables) {
+
+    valuables.addAll(listOfValuables);
+  }
+
+  @Override
+  public void retrieveValuables(ArrayList<Valuable> listOfValuables) {
+    valuables.removeAll(listOfValuables);
+  }
+
+  @Override
+  public ArrayList<Valuable> lookAtValuables() {
+    return valuables;
+  }
+
+  @Override
+  public String toString() {
+    return "TreasureRoom{" +
+            "valuables=" + valuables +
+            '}';
   }
 }
